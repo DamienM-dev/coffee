9dd880a02dd2600bcbba9698e16f9a3872af173f
DamienM-dev <84769945+DamienM-dev@users.noreply.github.com>
2022-02-17 11:21:01 +0100

    message

--- index.php
+++ index.php
@@ -3,8 +3,12 @@
 
 require('vendor/autoload.php');
 
+
+if($_SERVER['HTTP_HOST'] != "coffee-k6-dm.herokuapp.com") {
+
 $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+}
 
 function dbaccess() {
   $dbConnection = "mysql:dbname=". $_ENV['DB_NAME'] ."; host=". $_ENV['DB_HOST'] .":". $_ENV['DB_PORT'] ."; charset=utf8";
