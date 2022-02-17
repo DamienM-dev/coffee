1abf4458978292d09bd86502facffbcbb7bb6752
DamienM-dev <84769945+DamienM-dev@users.noreply.github.com>
2022-02-17 11:05:35 +0100

    'base'

--- index.php
+++ index.php
@@ -1,4 +1,41 @@
+<!-- CONNEXION A LA BDD -->
 <?php
-echo('hello world !');
 
-echo("vérification");
+require('vendor/autoload.php');
+
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+function dbaccess() {
+  $dbConnection = "mysql:dbname=". $_ENV['DB_NAME'] ."; host=". $_ENV['DB_HOST'] .":". $_ENV['DB_PORT'] ."; charset=utf8";
+  $user = $_ENV['DB_USERNAME'];
+  $pwd = $_ENV['DB_PASSWORD'];
+  
+  $db = new PDO ($dbConnection, $user, $pwd, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+  return $db;
+}
+  
+$db = dbaccess();
+
+$req = $db->query('SELECT name FROM waiter')->fetchAll();
+
+foreach ($req as $dbreq) {
+  echo $dbreq['name'] . "<br>";
+}
+
+var_dump($db);
+
+
+function waiters(){
+    global $db;
+    $req = $db->query('SELECT id, name FROM waiter');
+    $waiters = $req->fetchAll();
+    return $waiters;
+}
+
+$waiters = waiters();
+
+foreach($waiters as $waiter){
+    echo ($waiter['name']).PHP_EOL;
+}
